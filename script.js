66e09edb73f7abe42292a4c5eca2ea4437e4ddc4
Razi Montgomery <darkwolfrazi@yahoo.com>
2020-03-11 15:42:12 -0700

    answered all 4 questions

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("What worked best for me was when you became hands on with the lesson.")
+console.log("if i could make a change about the corse it would be to record each lesson's demo for references and given a link or website for anyone needing extra help understanding the material. ")
+console.log("No, i learned so much more than what was expected from the course.")
+console.log("Everything was new to me because i had never used or experimented with JavaScript.")
